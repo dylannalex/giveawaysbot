f24a138633dc689f2ec5b42959a21af18bd24a0e
Dylan Tintenfich <tintenfichdylan@gmail.com>
2021-12-03 20:23:37 -0300

    feat: add reddit last hour nft giveaway link

--- giveawaysbot/reddit/__main__.py
+++ giveawaysbot/reddit/__main__.py
@@ -1,5 +1,5 @@
 from sys import argv
-from giveawaysbot import message
+from giveawaysbot import tools
 from giveawaysbot.reddit import bot as reddit_bot
 from giveawaysbot.reddit import links
 from giveawaysbot.settings import GIVEAWAY_PARTICIPATIONS
@@ -8,7 +8,12 @@ from giveawaysbot.settings import GIVEAWAY_PARTICIPATIONS
 def start_bot(argv) -> None:
     username, password, wallet = argv[0], argv[1], argv[2]
     bot = reddit_bot.RedditBot(username, password)
-    giveaways = bot.search_giveaways(links.REDDIT_OPENSEA_LAST_HOUR_GIVEAWAYS_LINK)
+    giveaways = tools.remove_duplicates(
+        [
+            *bot.search_giveaways(links.REDDIT_OPENSEA_LAST_HOUR_GIVEAWAYS_LINK),
+            *bot.search_giveaways(links.REDDIT_LAST_HOUR_NFT_GIVEAWAY_LINK),
+        ]
+    )
     if not giveaways:
         return
     bot.login()
--- giveawaysbot/reddit/links.py
+++ giveawaysbot/reddit/links.py
@@ -1,3 +1,6 @@
 REDDIT_OPENSEA_LAST_HOUR_GIVEAWAYS_LINK = (
     "https://www.reddit.com/r/opensea/search/?q=giveaway&restrict_sr=1&t=hour&type=link"
 )
+REDDIT_LAST_HOUR_NFT_GIVEAWAY_LINK = (
+    "https://www.reddit.com/search/?q=nft%20giveaway&t=hour&sort=relevance"
+)
