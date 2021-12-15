6cd0341b5585e6d26f4018b15aa5c770241bb8b6
Dylan Tintenfich <tintenfichdylan@gmail.com>
2021-12-15 18:53:52 -0300

    fix: update button and textbox XPATH

--- giveawaysbot/reddit/bot.py
+++ giveawaysbot/reddit/bot.py
@@ -58,7 +58,7 @@ class RedditBot(SeleniumBot):
             EC.element_to_be_clickable(
                 (
                     By.XPATH,
-                    "//*[@id='SHORTCUT_FOCUSABLE_DIV']/div[2]/div/div/div/div[2]/div[3]/div[1]/div[2]/div[3]/div[2]/div/div/div[2]/div/div[1]/div/div/div",
+                    '//*[@id="SHORTCUT_FOCUSABLE_DIV"]/div[2]/div/div/div/div[2]/div[3]/div[1]/div[3]/div[3]/div[2]/div/div/div[2]/div/div[1]/div/div/div',
                 )
             )
         ).send_keys(message)
@@ -68,7 +68,7 @@ class RedditBot(SeleniumBot):
         sleep(settings.SLEEP_SECONDS)
         self.driver.find_element(
             By.XPATH,
-            "//*[@id='SHORTCUT_FOCUSABLE_DIV']/div[2]/div/div/div/div[2]/div[3]/div[1]/div[2]/div[3]/div[2]/div/div/div[3]/div[1]/button",
+            '//*[@id="SHORTCUT_FOCUSABLE_DIV"]/div[2]/div/div/div/div[2]/div[3]/div[1]/div[3]/div[3]/div[2]/div/div/div[3]/div[1]/button',
         ).click()
 
     @try_action(None, False)
